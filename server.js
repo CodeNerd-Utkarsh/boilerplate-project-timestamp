dc52d8ed604df94d81c69f50cf08d858fb7e4702
Utkarsh__Singh <utkarsh.singh.18@mountblue.tech>
2022-01-31 21:26:05 +0530

    completeed the task

--- server.js
+++ server.js
@@ -2,17 +2,18 @@
 // where your node app starts
 
 // init project
-var express = require('express');
-var app = express();
+let express = require('express');
+let app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+let cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 3030;
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -21,12 +22,29 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/:date", (req, res) => {
+  let userDate = (req.params.date);
+  if (!userDate.match(/-/g)) {
+    userDate = +userDate;
+    console.log(userDate);
+  }
+  let date = new Date(userDate);
+  if (date == "Invalid Date") {
+    res.json({ error: "Invalid Date" })
+  }
+  // to convert date into a utc format we can use =>
+  // date.toUTCString()
 
+  // to create date into unix userDate format we can use => date.valueOf()
+
+  res.json({ unix: date.valueOf(), utc: date.toUTCString() });
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+// process.env.PORT
+let listener = app.listen(PORT, function () {
+  console.log('Your app is running on http://localhost:' + listener.address().port);
 });
